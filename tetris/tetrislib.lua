8f92af3b68a2cbb0aa6bab4c420aaaa26957ae36
ayanamee <legollas4barreira@gmail.com>
2025-03-27 19:46:25 +0000

    working on tetris
    - scalable board border thickness and position

--- tetris/tetrislib.lua
+++ tetris/tetrislib.lua
@@ -1,35 +1,61 @@
---[[
 
-Tetromino : metatable for all tetrominoes
+--board variables
+h = 20*4
+w = 10*4
+thickness=2
+bx0= 1
+by0= 1
+x0= bx0+thickness
+y0= by0+thickness
 
-]]
+--tetromino variables
+speed = 1
+current_tet = {}
+
+--gravity
+gravity_buffer = 0
+default_g_force = 2
+high_g = 30
+g_force = default_g_force
+
+--Game States
+running = 0
 
 board = {}
 
-Tetromino = {sprite = 0, x=0, y=0, i=0,j=0, name='', rotation=0, alive=1} 
+Tetromino = {sprite = 0, x=x0, y=y0, idx=1, idy=1, name='', flipx=false, flipy=false, alive=1} 
 Tetromino.__index = Tetromino
 
 
 function init_board()
-
     for i=1, 20 do
         board[i]={}
         for j=1, 10 do
-            board[i][j] = {full = 0}
+            board[i][j] = {full = 0, x=x0+4*(j-1),y=y0+4*(i-1)}
         end
     end
 end
+
+function draw_board()
+    for i=1, 20 do
+        for j=1, 10 do
+            rectfill(board[i][j].x, board[i][j].y, board[i][j].x+3, 3+board[i][j].y,  5+ (i+j)%2)
+        end
+    end
+
+end
+
 
 
 function Tetromino:new(o)
     o = o or {}
     setmetatable(o, self)
-    o.rotation=0
-    o.x=0
-    o.y=0
-    o.i=0
-    o.j=0
+    o.rotation=1
+    o.idx=1
+    o.idy=1
     alive=1
+    flipx=false
+    flipy=false
     return o
 end
 
@@ -56,7 +82,8 @@ end
 
 
 function Tetromino:draw()
-    spr(self.sprite, self.x, self.y, 2, 2, flr(self.rotation/2) , self.rotation%2)
+    --spr(self.sprite, self.x, self.y, 2, 2, self.flipx , self.flipy)
+    spr(self.sprite, self.x, board[self.idy][self.idx].y, 2, 2, self.flipx , self.flipy)
 end
 
 function Tetromino:move(amount, left_bound, right_bound)
@@ -72,10 +99,10 @@ function Tetromino:gravity(force, lower_bound)
     if self.alive==1 then
         gravity_buffer+=force
         if(gravity_buffer>30) then
-            if self.y+4 >= lower_bound then
+            if self.idy == 20 then
                 self:kill_tet()
             else
-                self.y += 4
+                self.idy +=1
                 gravity_buffer=0
             end
         end
@@ -87,6 +114,9 @@ function Tetromino:kill_tet()
     add(dead_tets, self)
     current_tet=pop_tetromino(tet_list)
 end
+
+
+
 
 function pop_tetromino(tet_list)
     if tet_list[tet_list.index]!=0 then
